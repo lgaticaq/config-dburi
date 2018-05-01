7516a50a56f98a9a3b597dfb3f539ee1f45b8298
Leonardo Gatica <lgatica@protonmail.com>
2018-05-01 18:20:31 -0300

    fix(env): fix get uri from environment

--- src/index.js
+++ src/index.js
@@ -2,36 +2,19 @@
 
 const getMongodbUri = dbname => {
   dbname = dbname || 'test';
-  let uri = `mongodb://localhost/${dbname}`;
-  if (process.env.MONGO_PORT_27017_TCP_ADDR && process.env.MONGO_PORT_27017_TCP_PORT) {
-    uri = `mongodb://${process.env.MONGO_PORT_27017_TCP_ADDR}:${process.env.MONGO_PORT_27017_TCP_PORT}/${dbname}`;
-  } else if (process.env.MONGODB_URI) {
-    uri = process.env.MONGODB_URI;
-  }
-  return uri;
+  return process.env.MONGODB_URI || `mongodb://localhost/${dbname}`;
 };
 
 const getRedisUri = () => {
-  let uri = 'redis://localhost';
-  if (process.env.REDIS_PORT_6379_TCP_ADDR && process.env.REDIS_PORT_6379_TCP_PORT) {
-    uri = `redis://${process.env.REDIS_PORT_6379_TCP_ADDR}:${process.env.REDIS_PORT_6379_TCP_PORT}`;
-  } else if (process.env.REDIS_URI) {
-    uri = process.env.REDIS_URI;
-  }
-  return uri;
+  return process.env.REDIS_URI || 'redis://localhost';
 };
 
 const getPostgresqlUri = password => {
   const username = process.env.POSTGRES_USER || 'postgres';
   const dbname = process.env.POSTGRES_DB || process.env.POSTGRES_USER || 'postgres';
   password = password || process.env.POSTGRES_PASSWORD || 'mysecretpassword';
-  let uri = `postgres://${username}:${password}@localhost/${dbname}`;
-  if (process.env.POSTGRES_PORT_5432_TCP_ADDR && process.env.POSTGRES_PORT_5432_TCP_PORT && process.env.POSTGRES_PASSWORD) {
-    uri = `postgres://${username}:${password}@${process.env.POSTGRES_PORT_5432_TCP_ADDR}:${process.env.POSTGRES_PORT_5432_TCP_PORT}/${dbname}`;
-  } else if (process.env.POSTGRESQL_URI) {
-    uri = process.env.POSTGRESQL_URI;
-  }
-  return uri;
+  const uri = `postgres://${username}:${password}@localhost/${dbname}`;
+  return process.env.POSTGRESQL_URI || uri;
 };
 
 module.exports = {
