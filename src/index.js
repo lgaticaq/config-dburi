96b08b2066a1b48b4cdebc47eacc9f14e15505c5
Leonardo Gatica <lgatica@protonmail.com>
2018-05-01 18:29:39 -0300

    style(js): migrate to standardjs

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-'use strict';
+'use strict'
 
 const getMongodbUri = dbname => {
-  dbname = dbname || 'test';
-  return process.env.MONGODB_URI || `mongodb://localhost/${dbname}`;
-};
+  dbname = dbname || 'test'
+  return process.env.MONGODB_URI || `mongodb://localhost/${dbname}`
+}
 
 const getRedisUri = () => {
-  return process.env.REDIS_URI || 'redis://localhost';
-};
+  return process.env.REDIS_URI || 'redis://localhost'
+}
 
 const getPostgresqlUri = password => {
-  const username = process.env.POSTGRES_USER || 'postgres';
-  const dbname = process.env.POSTGRES_DB || process.env.POSTGRES_USER || 'postgres';
-  password = password || process.env.POSTGRES_PASSWORD || 'mysecretpassword';
-  const uri = `postgres://${username}:${password}@localhost/${dbname}`;
-  return process.env.POSTGRESQL_URI || uri;
-};
+  const username = process.env.POSTGRES_USER || 'postgres'
+  const dbname =
+    process.env.POSTGRES_DB || process.env.POSTGRES_USER || 'postgres'
+  password = password || process.env.POSTGRES_PASSWORD || 'mysecretpassword'
+  const uri = `postgres://${username}:${password}@localhost/${dbname}`
+  return process.env.POSTGRESQL_URI || uri
+}
 
 module.exports = {
   mongo: getMongodbUri,
   redis: getRedisUri,
   postgres: getPostgresqlUri
-};
+}
--- test/test.js
+++ test/test.js
@@ -1,46 +1,56 @@
-'use strict';
+'use strict'
 
-const config = require('../src');
-const expect = require('chai').expect;
+const config = require('../src')
+const { describe, it } = require('mocha')
+const { expect } = require('chai')
 
 describe('config-dburi', () => {
   describe('mongo', () => {
     it('should return mongo uri default', () => {
-      delete process.env.MONGODB_URI;
-      expect(config.mongo()).to.eql('mongodb://localhost/test');
-    });
+      delete process.env.MONGODB_URI
+      expect(config.mongo()).to.eql('mongodb://localhost/test')
+    })
     it('should return mongo uri from env default', () => {
-      process.env.MONGODB_URI = 'mongodb://user:pass@mongo.example.com:1234';
-      expect(config.mongo()).to.eql('mongodb://user:pass@mongo.example.com:1234');
-    });
-  });
+      process.env.MONGODB_URI = 'mongodb://user:pass@mongo.example.com:1234'
+      expect(config.mongo()).to.eql(
+        'mongodb://user:pass@mongo.example.com:1234'
+      )
+    })
+  })
 
   describe('redis', () => {
     it('should return redis uri default', () => {
-      delete process.env.REDIS_URI;
-      expect(config.redis()).to.eql('redis://localhost');
-    });
+      delete process.env.REDIS_URI
+      expect(config.redis()).to.eql('redis://localhost')
+    })
     it('should return redis uri from env default', () => {
-      process.env.REDIS_URI = 'redis://redis:pass@redis.example.com:1234';
-      expect(config.redis()).to.eql('redis://redis:pass@redis.example.com:1234');
-    });
-  });
+      process.env.REDIS_URI = 'redis://redis:pass@redis.example.com:1234'
+      expect(config.redis()).to.eql('redis://redis:pass@redis.example.com:1234')
+    })
+  })
 
   describe('postgres', () => {
     it('should return postgres uri default', () => {
-      delete process.env.POSTGRES_PASSWORD;
-      delete process.env.POSTGRESQL_URI;
-      expect(config.postgres()).to.eql('postgres://postgres:mysecretpassword@localhost/postgres');
-    });
+      delete process.env.POSTGRES_PASSWORD
+      delete process.env.POSTGRESQL_URI
+      expect(config.postgres()).to.eql(
+        'postgres://postgres:mysecretpassword@localhost/postgres'
+      )
+    })
     it('should return postgres uri default with password', () => {
-      delete process.env.POSTGRES_PASSWORD;
-      delete process.env.POSTGRESQL_URI;
-      expect(config.postgres('mypass')).to.eql('postgres://postgres:mypass@localhost/postgres');
-    });
+      delete process.env.POSTGRES_PASSWORD
+      delete process.env.POSTGRESQL_URI
+      expect(config.postgres('mypass')).to.eql(
+        'postgres://postgres:mypass@localhost/postgres'
+      )
+    })
     it('should return postgres uri from env default', () => {
-      delete process.env.POSTGRES_PASSWORD;
-      process.env.POSTGRESQL_URI = 'postgres://user:pass@postgres.example.com:1234';
-      expect(config.postgres()).to.eql('postgres://user:pass@postgres.example.com:1234');
-    });
-  });
-});
+      delete process.env.POSTGRES_PASSWORD
+      process.env.POSTGRESQL_URI =
+        'postgres://user:pass@postgres.example.com:1234'
+      expect(config.postgres()).to.eql(
+        'postgres://user:pass@postgres.example.com:1234'
+      )
+    })
+  })
+})
